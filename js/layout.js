5db081c63e39ced35605ac80166ddedc75406541
taedonn <taedonn@taedonn.com>
2023-11-02 23:36:45 +0900

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -136,4 +136,48 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
         </div>
     `;
+
+    // 답변 메일 제목
+    wrap.innerHTML += `
+        <div style="width:100%; height:8px; background-color:#DDD; margin: 60px 0;"></div>
+        <div style="width: 100%; max-width:520px; background-color: #FFF; margin:0 auto; padding: 20px 32px; box-sizing:border-box; margin-top: 60px; margin-bottom: 12px;">
+            <div style="font-size: 12px; font-weight: 400; color: #666; text-decoration: underline; text-underline-offset: 4px; margin-bottom: 8px;">"폰트 아카이브" &lt;taedonn@taedonn.com></div>
+            <div style="font-size: 18px; font-weight: 700; color: #333;">[폰트 아카이브] 제보해주신 내용에 대한 답변입니다.</div>
+        </div>
+    `;
+
+    // 답변 메일 내용
+    wrap.innerHTML += `
+        <div style="width:100%; font-size:16px; font-weight:400; line-height:1.25; color:#000; font-family:'Roboto', 'Noto Sans KR', '맑은고딕', Malgun Gothic, '돋움', Dotum, Helvetica, 'Apple SD Gothic Neo', Sans-serif;">
+            <div style="width:100%; max-width:520px; background-color:#FFF; margin:0 auto; padding:80px 20px; box-sizing:border-box; font-size:16px; font-weight:400; line-height:1.25; color:#000;">
+                <div style="width:100%; max-width:400px; margin:0 auto;">
+                    <div style="width: 100%; margin: 0 auto;">
+                        <div style="margin: 0 auto; width:24px; height:24px; background-color:#000; color:#FFF; font-size:10px; font-weight:400; text-align:center; line-height:22px; border-radius:4px;">Aa</div>
+                        <div style="margin: 0 auto; margin-top: 16px; font-size: 14px; text-align: center; color: #3A3A3A;">폰트 아카이브</div>
+                    </div>
+                    <h2 style="font-size:20px; font-weight:500; text-align: center; color: #3A3A3A; margin-top:8px;">
+                        제보해주신 내용에 대한 <span style="color: #000; font-weight: 700;">답변입니다.</span>
+                    </h2>
+                    <p style="width:100%; font-size:14px; font-weight:500; line-height:2; color:#3A3A3A; text-decoration:underline; margin:0; margin-top:40px;">
+                        제보
+                    </p>
+                    <div style="width:100%; padding:16px 20px; box-sizing:border-box; margin-top:8px; box-sizing:border-box; background-color:#EEE; font-size:12px; font-weight:500; color:#3A3A3A; text-decoration:none; border-radius:6px;">
+                        ${"제목"}
+                    </div>
+                    <p style="width:100%; font-size:14px; font-weight:500; line-height:2; color:#3A3A3A; text-decoration:underline; margin:0; margin-top:28px;">
+                        답변
+                    </p>
+                    <div style="width:100%; padding:16px 20px; box-sizing:border-box; margin-top:8px; box-sizing:border-box; background-color:#EEE; font-size:12px; font-weight:500; color:#3A3A3A; text-decoration:none; border-radius:6px;">
+                        ${"내용"}
+                    </div>
+                    <div style="width:100%; height:1px; background-color:#EEE; margin-top:48px;"></div>
+                    <p style="width:100%; font-size:12px; font-weight:400; line-height:2.5; color:#97989C; margin:0; margin-top:24px;">
+                        taedonn - <a style="text-decoration:none; color:#067DF7;" href="https://fonts.taedonn.com">fonts.taedonn.com</a> <br/>
+                        check our GitHub repository @<a style="text-decoration:none; color:#97989C;" href="https://github.com/taedonn/fonts-archive">github.com/taedonn/fonts-archive</a> <br/>
+                        © 2023. taedonn, all rights reserved.
+                    </p>
+                </div>
+            </div>
+        </div>
+    `;
 });
